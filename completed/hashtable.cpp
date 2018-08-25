a4b59dda87922ae03830d44c63cd00b3ea947b16
aton <andrewton@verizon.net>
2018-08-25 17:51:14 -0400

    working on c++ impl of hashtable

---
+++ completed/hashtable.cpp
@@ -0,0 +1,18 @@
+/*
+FEATURES
+Open addressing (linear probing)
+hashing fxn is a power of 37
+Why is table size prime? --> reduces possible collisions if the tablesize & the hashing functions are coprime!
+
+future implementation:
+1. resize automatically if half full --> reduces collisions
+2. quadratic probing --> more spread
+3. implement delete (requires you to have sentinel values (easiest))
+*/
+
+int main() {
+
+	return 0;
+}
+
+class
